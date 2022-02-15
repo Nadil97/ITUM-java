cb76e8806296c530f044947c8131e56af4151376
Nadil Sandaruwan <nadilsandaruwan97@gmail.com>
2022-02-15 19:29:11 +0530

    Added java oop exmple

--- src/com/nadil/Main.java
+++ src/com/nadil/Main.java
@@ -3,6 +3,13 @@ package com.nadil;
 public class Main {
 
     public static void main(String[] args) {
+        ABC obj =new ABC();
         System.out.println("Hello world");
+    }
+}
+
+class ABC{
+    public ABC(){
+        System.out.println("THis is a Constructer");
     }
 }
